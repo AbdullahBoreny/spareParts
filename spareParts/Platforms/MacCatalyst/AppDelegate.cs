b24b49007be9aa28a8d27af90751f7d613f7e53e
Abdallah <Abdallah@DESKTOP-4JT0VDJ>
2025-10-27 17:35:40 +0300

    Add project files.

---
+++ spareParts/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace spareParts
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
---
+++ spareParts/Utilities(network)/NetworkCheck.cs
@@ -0,0 +1,8 @@
+﻿namespace spareParts.Utilities_network;
+public static class NetworkCheck
+{
+    public static bool IsConnected()
+    {
+        return Connectivity.Current.NetworkAccess == NetworkAccess.Internet;
+    }
+}
