7a1f58166bb8bb1d85a932abc314ca926b0fc62c
Abdallah <Abdallah@DESKTOP-4JT0VDJ>
2025-12-21 22:33:35 +0300

    Reapply "rename columns ,table. add constraints to reference security table"
    This reverts commit ab12eeb271194403936adda4946d0f68f6082c62.

--- SpareParts.Database/Communication/Messages.sql
+++ SpareParts.Database/Communication/Messages.sql
@@ -1,16 +1,11 @@
 ﻿CREATE TABLE [Communication].[Messages]
 (
     Id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
-
-    ConversationId INT NOT NULL,
-    SenderId INT NOT NULL,
-
+    ConversationId INT NOT NULL,          
+    SenderId UNIQUEIDENTIFIER NOT NULL,    
     Content NVARCHAR(MAX) NOT NULL,
-    CreatedAt DATETIME2 NOT NULL DEFAULT GETDATE(),
-
+    CreatedAt DATETIME NOT NULL DEFAULT GETDATE(),
     IsRead BIT NOT NULL DEFAULT 0,
-
-    CONSTRAINT FK_Messages_Conversations
-        FOREIGN KEY (ConversationId)
-        REFERENCES [Communication].[Conversations](Id)
+    FOREIGN KEY (ConversationId) REFERENCES [Communication].[Conversation](Id),
+    FOREIGN KEY (SenderId) REFERENCES [Security].[Users](UserID)
 );
