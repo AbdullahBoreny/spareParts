113fd433c83e9cdb90611625ff79f2719884511b
Abdallah <Abdallah@DESKTOP-4JT0VDJ>
2025-12-21 22:23:25 +0300

    rename columns, add constraints to reference security table

--- SpareParts.Database/Communication/Conversation.sql
+++ SpareParts.Database/Communication/Conversation.sql
@@ -1,12 +1,9 @@
-﻿CREATE TABLE [Communication].[Conversations]
+﻿CREATE TABLE [Communication].[Conversation]
 (
     Id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
-
-    User1Id INT NOT NULL,
-    User2Id INT NOT NULL,
-
-    CreatedAt DATETIME2 NOT NULL DEFAULT GETDATE(),
-
-    CONSTRAINT CHK_Users_Not_Same CHECK (User1Id <> User2Id),
-    CONSTRAINT UQ_User_Pair UNIQUE (User1Id, User2Id)
+    CustomerId UNIQUEIDENTIFIER NOT NULL,
+    ShopOwnerId UNIQUEIDENTIFIER NOT NULL, 
+    CreatedAt DATETIME NOT NULL DEFAULT GETDATE(),
+    FOREIGN KEY (CustomerId) REFERENCES [Security].[Users](UserID),
+    FOREIGN KEY (ShopOwnerId) REFERENCES [Security].[Users](UserID)
 );
