8d580867dd982e273194ddf01aedb92ad08774cc
Abdallah <Abdallah@DESKTOP-4JT0VDJ>
2025-12-21 20:57:39 +0300

    Completed Conversation and Messages tables structures.

--- SpareParts.Database/Communication/Conversation.sql
+++ SpareParts.Database/Communication/Conversation.sql
@@ -1,4 +1,12 @@
-﻿CREATE TABLE [Communication].[Conversation]
+﻿CREATE TABLE [Communication].[Conversations]
 (
-	[Id] INT NOT NULL PRIMARY KEY
-)
+    Id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+
+    User1Id INT NOT NULL,
+    User2Id INT NOT NULL,
+
+    CreatedAt DATETIME2 NOT NULL DEFAULT GETDATE(),
+
+    CONSTRAINT CHK_Users_Not_Same CHECK (User1Id <> User2Id),
+    CONSTRAINT UQ_User_Pair UNIQUE (User1Id, User2Id)
+);
--- SpareParts.Database/Communication/Messages.sql
+++ SpareParts.Database/Communication/Messages.sql
@@ -1,4 +1,16 @@
 ﻿CREATE TABLE [Communication].[Messages]
 (
-	[Id] INT NOT NULL PRIMARY KEY
-)
+    Id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+
+    ConversationId INT NOT NULL,
+    SenderId INT NOT NULL,
+
+    Content NVARCHAR(MAX) NOT NULL,
+    CreatedAt DATETIME2 NOT NULL DEFAULT GETDATE(),
+
+    IsRead BIT NOT NULL DEFAULT 0,
+
+    CONSTRAINT FK_Messages_Conversations
+        FOREIGN KEY (ConversationId)
+        REFERENCES [Communication].[Conversations](Id)
+);
